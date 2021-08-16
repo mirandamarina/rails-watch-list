fef24380c07b865eaf074790353597eb171a088e
Marina Miranda <mirandaxmarina@gmail.com>
2021-08-16 11:24:30 +0200

    latest

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,18 @@
 class BookmarksController < ApplicationController
   def new
+    @bookmark = Bookmark.new
   end
 
   def create
+    Bookmark.create(def_params)
+    @list = List.find(params[:list_id])
+    @list = @bookmark.list
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def def_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,30 @@
 class ListsController < ApplicationController
   def index
+    @lists = List.all
+    @list = List.new
   end
 
   def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+    # redirect_to list_path
   end
 
   def new
   end
 
   def create
+    List.create(list_params)
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
